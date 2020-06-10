d72c3e41c9c69d6ea9744a896e7f254f8dda1d3b
Mohamed Bouziani <mohamedbouziani25@gmail.com>
2020-06-10 22:14:45 +0100

    Create PickContext.kt

---
+++ android/src/main/kotlin/info/danielmartinez/fcontacts/PickContext.kt
@@ -0,0 +1,14 @@
+package info.danielmartinez.fcontacts
+
+import android.app.Activity
+import io.flutter.plugin.common.PluginRegistry
+
+interface PickContext : PluginRegistry.ActivityResultListener {
+
+    val activity: Activity
+
+    fun removeActivityResultListener(listener: PluginRegistry.ActivityResultListener)
+
+    fun addActivityResultListener(listener: PluginRegistry.ActivityResultListener)
+
+}
