4e5832d5663411cefac322a83b6479a290f8c564
Mohamed Bouziani <mohamedbouziani25@gmail.com>
2020-06-11 12:55:20 +0100

    Update FContactsPlugin.kt

--- android/src/main/kotlin/info/danielmartinez/fcontacts/FContactsPlugin.kt
+++ android/src/main/kotlin/info/danielmartinez/fcontacts/FContactsPlugin.kt
@@ -24,21 +24,13 @@ class FContactsPlugin (
       val channel = MethodChannel(registrar.messenger(), "fcontacts")
       channel.setMethodCallHandler(FContactsPlugin( registrar.context().contentResolver, registrar.context() ))
     }
-      const val PICK_PHONE = 2015
-      const val PICK_EMAIL = 2020
-      const val FLUTTER_CONTACT_PICKER = "info.danielmartinez.fcontacts"
+   
   }
-    private var channel1: MethodChannel? = null
-    private var activity: ActivityPluginBinding? = null
-    private val context1: PickContext = V2Context()
-
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     when {
       call.method == "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
       call.method == "list" -> list( result, call.argument("query") )
-      call.method == "myfunc" -> myfunc(result)
-      call.method == "pickPhoneContact" -> ContactPicker.requestPicker(PICK_PHONE, ContactsContract.CommonDataKinds.Phone.CONTENT_URI, result, context1)
       else -> result.notImplemented()
     }
   }
@@ -49,37 +41,4 @@ class FContactsPlugin (
     }
   }
     
-    private fun myfunc( result: Result) {
-      result.success( "this is my string, i called it" )
-  }
-    
-  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel1 = MethodChannel(binding.binaryMessenger, FLUTTER_CONTACT_PICKER)
-        channel1?.setMethodCallHandler(this)
-    }
-
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel1?.setMethodCallHandler(null)
-        channel1 = null
-    }
-
-    override fun onDetachedFromActivity() {
-        activity?.removeActivityResultListener(context1)
-        activity = null
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) = onAttachedToActivity(binding)
-
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        binding.addActivityResultListener(context1)
-        activity = binding
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() = onDetachedFromActivity()
-
-    private inner class V2Context : AbstractPickContext() {
-        override val activity: Activity
-            get() = this@FlutterContactPickerPlugin.activity?.activity ?: error("No Activity")
-
-    }
 }
